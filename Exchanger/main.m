d7ee26f590fe477a951a21a31c1563c35c9720cf
Vladimir Kaltyrin <vkaltyrin@avito.ru>
2017-07-10 12:37:53 +0300

    Add xcode project

---
+++ Exchanger/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Exchanger
+//
+//  Created by Калтырин Владимир on 10.07.17.
+//  Copyright © 2017 Vladimir Kaltyrin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
