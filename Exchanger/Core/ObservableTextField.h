3cfcf4e4cd22ff225599efb26b86d0f5ed43e5e5
Vladimir Kaltyrin <vkaltyrin@avito.ru>
2017-12-12 12:28:57 +0300

    Use ObservableTextField in GalleryPreviewPage

--- Exchanger/Core/ObservableTextField.h
+++ Exchanger/Core/ObservableTextField.h
@@ -1,14 +1,15 @@
 #import <UIKit/UIKit.h>
 #import "TextFieldConfiguration.h"
 
+typedef NSAttributedString *(^TextFieldAttributedStringFormatter)(NSString *text);
+
 @interface ObservableTextField : UIView
 @property (nonatomic, strong) BOOL(^onTextChange)(NSString *text);
+@property (nonatomic, strong) TextFieldAttributedStringFormatter formatter;
 
 - (instancetype)initWithFrame:(CGRect)frame __attribute__((unavailable("initWithFrame not available")));
 
 - (void)setConfiguration:(TextFieldConfiguration *)configuration;
-
-- (void)setAttributedText:(NSAttributedString *)text;
 
 - (void)setText:(NSString *)text;
 
