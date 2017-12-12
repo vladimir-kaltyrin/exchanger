d11e467c6d5b2057e300a08584609a06fe309e87
Vladimir Kaltyrin <vkaltyrin@avito.ru>
2017-12-12 19:17:54 +0300

    Clean code in ExchangeMoneyViewDataBuilder

--- Exchanger/Views/GalleryPreviewView/CarouselData.h
+++ Exchanger/Views/GalleryPreviewView/CarouselData.h
@@ -7,12 +7,10 @@ NS_ASSUME_NONNULL_BEGIN
 @interface CarouselData : NSObject
     
 @property (nonatomic, strong, readonly) NSArray<CarouselPageData *> *pages;
-@property (nonatomic, strong, readonly, nullable) void(^onTap)();
 @property (nonatomic, assign, readonly) NSInteger currentPage;
     
 - (instancetype)initWithPages:(NSArray<CarouselPageData *> *)pages
-                  currentPage:(NSInteger)currentPage
-                        onTap:(void(^)())onTap;
+                  currentPage:(NSInteger)currentPage;
     
 @end
 
