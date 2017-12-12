811351339963d17bb703976e5a3fa5ec154eb208
Vladimir Kaltyrin <vkasci@gmail.com>
2017-12-12 13:55:46 +0300

    Don't use C struct to hold parse data

--- Exchanger/Formatters/BalanceFormatter/BalanceParseData.m
+++ Exchanger/Formatters/BalanceFormatter/BalanceParseData.m
@@ -1,11 +1,3 @@
-//
-//  BalanceParseData.m
-//  Exchanger
-//
-//  Created by Калтырин Владимир on 12.12.17.
-//  Copyright © 2017 Vladimir Kaltyrin. All rights reserved.
-//
-
 #import "BalanceParseData.h"
 
 @implementation BalanceParseData
---
+++ Exchanger/Formatters/BalanceFormatter/FormattedStringData.h
@@ -0,0 +1,6 @@
+#import <Foundation/Foundation.h>
+
+@interface FormattedStringData : NSObject
+@property (nonatomic, strong) NSString *primaryString;
+@property (nonatomic, strong) NSString *secondaryString;
+@end
