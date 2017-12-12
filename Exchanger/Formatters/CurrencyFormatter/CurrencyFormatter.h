3dca33c7e3c53a1b532f120669044c659edca114
Vladimir Kaltyrin <vkasci@gmail.com>
2017-12-12 19:32:04 +0300

    Fix nullability for CurrencyFormatter

--- Exchanger/Formatters/CurrencyFormatter/CurrencyFormatter.h
+++ Exchanger/Formatters/CurrencyFormatter/CurrencyFormatter.h
@@ -1,7 +1,7 @@
 #import "CurrencyType.h"
 
 @protocol CurrencyFormatter
-- (nonnull NSString *)toCodeString:(CurrencyType)currencyType;
+- (nullable NSString *)toCodeString:(CurrencyType)currencyType;
 - (nonnull NSString *)toSignString:(CurrencyType)currencyType;
 - (CurrencyType)currencyTypeFromString:(nonnull NSString *)string;
 @end
