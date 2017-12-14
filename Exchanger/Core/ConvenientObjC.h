8a32075a9ba556ee2a1f23fa1532e51646081d69
Vladimir Kaltyrin <vkaltyrin@avito.ru>
2017-12-14 15:01:58 +0300

    Add EXTScope

--- Exchanger/Core/ConvenientObjC.h
+++ Exchanger/Core/ConvenientObjC.h
@@ -2,6 +2,10 @@
 
 #import <ObjectiveSugar/ObjectiveSugar.h>
 
+// @weakify, @strongify
+
+#import "EXTScope.h"
+
 // Safe block syntax
 
 #define safeBlock(block, ...) if (block != nil) { block(__VA_ARGS__); }
