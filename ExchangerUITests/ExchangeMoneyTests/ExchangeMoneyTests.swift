4a8faae9d855f722a44a6f79febd8e1db5bfaf6e
Vladimir Kaltyrin <vkasci@gmail.com>
2017-12-15 21:33:02 +0300

    Add empty test case for UI test

---
+++ ExchangerUITests/ExchangeMoneyTests/ExchangeMoneyTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+
+class ExchangeMoneyTests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        continueAfterFailure = false
+        
+        //XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+    }
+    
+}
