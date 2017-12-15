416c050136078f82535040ce2b0328799967115a
Vladimir Kaltyrin <vkasci@gmail.com>
2017-12-15 21:24:23 +0300

    Add OHHTTPStubs/Swift

--- Pods/Target Support Files/Pods-Exchanger/Pods-Exchanger-frameworks.sh
+++ Pods/Target Support Files/Pods-Exchanger/Pods-Exchanger-frameworks.sh
@@ -87,6 +87,17 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ObjectiveSugar/ObjectiveSugar.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/XMLDictionary/XMLDictionary.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ObjectiveSugar/ObjectiveSugar.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/XMLDictionary/XMLDictionary.framework"
+fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
