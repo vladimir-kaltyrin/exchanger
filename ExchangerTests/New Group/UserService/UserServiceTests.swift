8356ca8dbefa70c1bd738db7076a2ea55f2d526a
Vladimir Kaltyrin <vkaltyrin@avito.ru>
2017-12-16 16:03:39 +0300

    Add tests for ExchangeRatesService

---
+++ ExchangerTests/New Group/UserService/UserServiceTests.swift
@@ -0,0 +1,49 @@
+import XCTest
+
+class UserServiceTests: TestCase {
+    
+    var userService: UserService!
+    
+    override func setUp() {
+        super.setUp()
+        
+        userService = ServiceFactoryImpl.instance().userService()
+    }
+    
+    func testInitialUser() {
+        // Given
+        let callbackCalled = expectation(description: "callback is called")
+        
+        // When
+        var expectedUser: User?
+        userService.currentUser { user in
+            expectedUser = user
+            callbackCalled.fulfill()
+        }
+        
+        // then
+        waitForExpectations(timeout: timeout) { _ in
+            if let eurWallet = expectedUser?.wallet(with: .EUR) {
+                XCTAssertTrue(eurWallet.currency.currencyType == .EUR)
+                XCTAssertTrue(eurWallet.amount == 100)
+            } else {
+                XCTFail()
+            }
+            
+            if let usdWallet = expectedUser?.wallet(with: .USD) {
+                XCTAssertTrue(usdWallet.currency.currencyType == .USD)
+                XCTAssertTrue(usdWallet.amount == 100)
+            } else {
+                XCTFail()
+            }
+            
+            if let gbpWallet = expectedUser?.wallet(with: .GBP) {
+                XCTAssertTrue(gbpWallet.currency.currencyType == .GBP)
+                XCTAssertTrue(gbpWallet.amount == 100)
+            } else {
+                XCTFail()
+            }
+        }
+    }
+    
+}
