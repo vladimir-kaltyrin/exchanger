b99d907e8048ed32cea18cc25f5b2e09c0070661
Vladimir Kaltyrin <vkasci@gmail.com>
2017-12-16 16:03:39 +0300

    Add tests for ExchangeRatesService

---
+++ ExchangerTests/New Group/ExchangeRatesService/ExchangeRatesServiceTests.swift
@@ -0,0 +1,59 @@
+import XCTest
+import OHHTTPStubs
+
+class ExchangeRatesServiceTests: TestCase {
+    
+    private let urlString = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"
+    
+    var service: ExchangeRatesService!
+    
+    override func setUp() {
+        super.setUp()
+        
+        service = ServiceFactoryImpl.instance().exchangeRatesService()
+    }
+    
+    func testFetchRatesRequestAndParsing() {
+        // Given
+        
+        // given
+        stub(condition: isPath(urlString)) { _ in
+            return OHHTTPStubsResponse(
+                fileAtPath: OHPathForFile("rates.xml", type(of: self))!,
+                statusCode: 200,
+                headers: ["Content-Type":"application/xml"]
+            )
+        }
+        
+        let callbackCalled = expectation(description: "callback is called")
+        
+        // When
+        var expectedData: ExchangeRatesData?
+        service.fetchRates ({ data in
+            
+            expectedData = data
+            
+            callbackCalled.fulfill()
+            
+        }, onError: { _ in })
+        
+        // then
+        waitForExpectations(timeout: timeout) { _ in
+            if let expectedData = expectedData {
+                let currencies = expectedData.currencies
+                let usd = currencies?.first(where: { currency in
+                    return currency.currencyType == .USD
+                })
+                XCTAssertTrue(usd?.rate.floatValue == 1.1806)
+                
+                let gbp = currencies?.first(where: { currency in
+                    return currency.currencyType == .GBP
+                })
+                XCTAssertTrue(gbp?.rate.floatValue == 0.88253)
+                
+            } else {
+                XCTFail()
+            }
+        }
+    }
+}
