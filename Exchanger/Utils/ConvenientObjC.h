f730966e2597710340fb3ca4d082062178feea41
Vladimir Kaltyrin <vkaltyrin@avito.ru>
2017-12-19 13:31:08 +0300

    Remove EXTScope

--- Exchanger/Utils/ConvenientObjC.h
+++ Exchanger/Utils/ConvenientObjC.h
@@ -2,10 +2,6 @@
 
 #import <ObjectiveSugar/ObjectiveSugar.h>
 
-// @weakify, @strongify
-
-#import "EXTScope.h"
-
 // Safe block syntax
 
 #define safeBlock(block, ...) if (block != nil) { block(__VA_ARGS__); }
